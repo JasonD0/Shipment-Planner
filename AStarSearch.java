badfb6f38cbb862173a66335504a4e863f951768
Jason Do <Jason.Do1@hotmail.com>
2018-04-11 14:40:56 +1000

    starter code for A* search alg

---
+++ AStarSearch.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class AStarSearch<Node> {
+
+	private Node startNode;
+	private Node endNode;
+	
+	private PriorityQueue<Node> openSet = new PriorityQueue<Node>(); 
+	private HashSet<Node> closedSet = new HashSet<Node>();
+	private HashMap<Integer, Node> cameFrom = new HashMap<Integer, Node>();
+	private Map<Node, Double> cost = new HashMap<Node, Double>();  // SELF NOTE: Double parameter can be null, double can't (similar with int)
+	// pretend null is infinity
+	
+	
+	
+	public AStarSearch(Node startNode, Node endNode) {
+		this.startNode = startNode;
+		this.endNode = endNode;
+	}
+	
+	public void getPath() {
+		
+	}
+}
---
+++ Node.java
@@ -0,0 +1,15 @@
+// each node is a 'point in graph'
+
+public class Node {
+	private int nodeId;
+	
+	public Node(int id) {
+		this.nodeId = id;
+	}
+	
+	public int getNodeId() {
+		return this.nodeId;
+	}
+	
+	
+}
